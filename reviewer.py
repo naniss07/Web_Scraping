33d8afa05de99fc86214d1eb34fea815a28ddfcf
naniss07 <naniszkocagoz@gmail.com>
2025-01-19 22:33:27 +0300

    commit

--- reviewer.py
+++ reviewer.py
@@ -48,12 +48,16 @@ def initialize_driver():
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
-        # Automatically use the correct version of ChromeDriver
+        # ChromeDriverManager'ı doğrudan kullan
+        service = ChromeService()
         driver = webdriver.Chrome(
-            service=ChromeService(ChromeDriverManager().install()), options=options
+            service=service,
+            options=options
         )
+        
         st.write("Chrome WebDriver initialized.")
         return driver
     except Exception as e:
